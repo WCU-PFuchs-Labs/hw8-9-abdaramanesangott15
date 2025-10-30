93421887a75bd1a957f7074154e5cb1d0b0fc588
abdaramanesangott15 <abdaramanesangott15@gmail.com>
2025-10-30 15:35:48 -0700

    Create Mult.java

---
+++ src/gp/Mult.java
@@ -0,0 +1,7 @@
+package gp;
+public final class Mult extends Binop {
+    public Mult(Node l, Node r) { super(l, r); }
+    protected double apply(double a, double b) { return a * b; }
+    protected String symbol() { return "*"; }
+    protected Binop create(Node l, Node r) { return new Mult(l, r); }
+}
